296e06689704c03f0822a5e9b90394f84848adeb
dudups5 <drive.dudu@gmail.com>
2025-03-19 15:14:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 from datetime import datetime
 
+# Fazendo a requisição para obter os valores das cotações
 requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-
 requisicao_dic = requisicao.json()
+
+# Pegando os valores das cotações
 cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
 cotacao_euro = requisicao_dic["EURBRL"]["bid"]
 cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
 
+# Exibindo as cotações na tela
 print(f"Cotação Atualizada. {datetime.now()}\nDólar: R${cotacao_dolar}\nEuro: R${cotacao_euro}\nBTC: R${cotacao_btc}")
